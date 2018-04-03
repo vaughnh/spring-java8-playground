eee8d4046b4b956b8376227d25f2fd9ebea9766b
Vaughn Harmon <vaughn.harmon@gmail.com>
2018-04-03 10:07:02 -0500

    Altered the Application class to derive from SpringSandboxApplication. This will support traditional deployments on Apache Tomcat on AWS Elastic Beanstalk.

--- src/main/java/org/boxtree/SpringSandboxApplication.java
+++ src/main/java/org/boxtree/SpringSandboxApplication.java
@@ -2,9 +2,10 @@ package org.boxtree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringSandboxApplication {
+public class SpringSandboxApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSandboxApplication.class, args);
